c254adecf27cd539e7232a224f6d6afbdc624af8
GallardoAlberto <gallardo.alb.1998@gmail.com>
2023-03-23 20:19:49 +0100

    menu movil

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,40 @@
 let cajaMenu = document.querySelector(".inicio--menu");
 let menu = cajaMenu.querySelector("ul");
 let menuMovil = document.querySelector(".menuMovil");
-let cerrarMenu = document.querySelector(".cerrarMenu");
 let logo = document.querySelector(".logoFijo");
-
+let cajaMovil = document.querySelector(".inicioMovil");
 let clickMenu = false;
 
 document.addEventListener("scroll", () =>{
+
     if(window.scrollY >= window.innerHeight){
+
         if(clickMenu == false){
-            menuMovil.style.right = "50px";
+
+            if(window.innerWidth <= 540){
+                menuMovil.style.right = "20px";
+            }else{
+                menuMovil.style.right = "50px";
+            }
+
         }else{
             menuMovil.style.right = "123px";
         }
         
-        logo.style.top = "30px";
-        logo.style.left = "0px";
+        if(window.innerWidth <= 540){
+            menuMovil.style.right = "20px";
+            logo.style.top = "0px";
+            logo.style.left = "0px";
+            cajaMovil.style.top = "0px";
+            menuMovil.style.top = "20px";
+
+        }else{
+            logo.style.top = "30px";
+            logo.style.left = "0px";
+            menuMovil.style.top = "50px";
+
+        }
+
 
     }else{
         menuMovil.classList.remove("fa-xmark");
@@ -32,13 +51,21 @@ document.addEventListener("scroll", () =>{
         menu.style.justifyContent = "center";
         logo.style.left = "-120px";
         clickMenu = false;
+        cajaMovil.style.top = "-100px";
+
     }
 })
 
 menuMovil.addEventListener("click" , () =>{
 
     if(clickMenu == false){
-        menuMovil.style.right = "123px";
+        if(window.innerWidth <= 540){
+            cajaMenu.style.width = "100%";
+            menuMovil.style.right = "20px";
+        }else{
+            cajaMenu.style.width = "300px";
+            menuMovil.style.right = "123px";
+        }
         menuMovil.style.backgroundColor = "transparent";
         menuMovil.style.boxShadow = "none";
         menuMovil.classList.remove("fa-bars");
@@ -47,7 +74,6 @@ menuMovil.addEventListener("click" , () =>{
         cajaMenu.style.position = "fixed";
         cajaMenu.style.top = "0";
         cajaMenu.style.right = "0";
-        cajaMenu.style.width = "300px";
         cajaMenu.style.height = "100vh";
         menu.style.position = "relative";
         menu.style.top = "125px";
@@ -55,15 +81,19 @@ menuMovil.addEventListener("click" , () =>{
         cajaMenu.style.backgroundColor = "black";
         clickMenu = true;
     }else{
-        menuMovil.style.right = "50px";
-        menuMovil.style.backgroundColor = "#2F2719";
-        menuMovil.style.boxShadow = "inset 0px -5px 1px 1px #573a1d, 0px 3px 1px 1px black, 0px -1px 1px 1px black";
+        if(window.innerWidth <= 540){
+            menuMovil.style.right = "20px";
+        }else{
+            menuMovil.style.right = "50px";
+            menuMovil.style.boxShadow = "inset 0px -5px 1px 1px #573a1d, 0px 3px 1px 1px black, 0px -1px 1px 1px black";
+            menuMovil.style.backgroundColor = "#2F2719";
+        }        
         menuMovil.classList.remove("fa-xmark");
         menuMovil.classList.add("fa-bars");
         cajaMenu.style.opacity = "1";
         cajaMenu.style.position = "fixed";
         cajaMenu.style.top = "0";
-        cajaMenu.style.right = "-300px";
+        cajaMenu.style.right = "-100%";
         cajaMenu.style.width = "300px";
         cajaMenu.style.height = "100vh";
         menu.style.position = "relative";
@@ -72,4 +102,5 @@ menuMovil.addEventListener("click" , () =>{
         clickMenu = false;
     }
     
+
 });
